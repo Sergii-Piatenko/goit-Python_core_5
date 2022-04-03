2016ae4a788a7d6fee9f71511e78d020bdd79de9
Serhii-Piatenko <94806482+Sergii-Piatenko@users.noreply.github.com>
2022-04-03 18:55:14 +0300

    Fixed deletion

--- hw6/sort.py
+++ hw6/sort.py
@@ -4,10 +4,16 @@ import shutil
 import sys
 
 
-def delete_empty_folder(path):
+def delete_folder(path):
 
-    for i in path:
-        os.rmdir(i)
+    important_folders = ('images', 'video', 'documents',
+                         'audio', 'archives', 'unknown extensions')
+
+    get_items = os.listdir(path)
+
+    for folder in get_items:
+        if not folder in important_folders:
+            shutil.rmtree(os.path.join(path, folder))
 
 
 def normalize(file_name):
@@ -16,24 +22,16 @@ def normalize(file_name):
     name, extension = os.path.splitext(file_name)
 
     for letter in name:
-
-        for key, value in TRANS.items():
-
-            if letter.isdigit():
-                result += letter
-                break
-
-            elif re.findall(r'[A-z]', letter):
-                result += letter
-                break
-
-            elif not re.findall(r'[А-яёЁіІїЇґҐ]', letter):
-                result += '_'
-                break
-
-            elif ord(letter) == key:
-                result += value
-                break
+        if letter.isdigit() or re.findall(r'[A-z]', letter):
+            result += letter
+        else:
+            for key, value in TRANS.items():
+                if not re.findall(r'[А-яёЁґҐ]', letter):
+                    result += '_'
+                    break
+                elif ord(letter) == key:
+                    result += value
+                    break
 
     return result + extension
 
@@ -45,59 +43,63 @@ def sorted_files(path):
                'documents': ('DOC', 'DOCX', 'TXT', 'PDF', 'XLS', 'XLSX', 'PPTX', 'PPT'),
                'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
                'archives': ('ZIP', 'GZ', 'TAR', 'RAR'), }
+    unknown_extensions = True
+    name_file = os.path.basename(path)
+    file_path = os.path.dirname(path)
+    end_folder = os.path.split(file_path)[-1]
+
+    if re.findall(r'[А-яёЁґҐ]+', name_file):
+        name_file = normalize(name_file)
 
     for folder, extension in folders.items():
 
+        if os.path.join(path_root, folder) in path:
+            unknown_extensions = False
+            continue
+
         if path.upper().endswith(extension):
 
-            name_file = os.path.basename(path)
-            file_path = os.path.dirname(path)
-            end_folder = os.path.split(file_path)[-1]
-
-            if re.findall(r'[А-яёЁґҐ]+', name_file):
-                name_file = normalize(name_file)
-
-            if end_folder == folder:
-                folder_destination = file_path
+            folder_destination = os.path.join(path_root, folder)
+            try:
+                os.mkdir(folder_destination, mode=0o777, dir_fd=None)
                 shutil.move(path, os.path.join(
                     folder_destination, name_file))
-            else:
-                folder_destination = os.path.join(file_path, folder)
-                try:
-                    os.mkdir(folder_destination, mode=0o777, dir_fd=None)
-                    shutil.move(path, os.path.join(
-                        folder_destination, name_file))
-                except FileExistsError:
-                    shutil.move(path, os.path.join(
-                        folder_destination, name_file))
+            except FileExistsError:
+                shutil.move(path, os.path.join(
+                    folder_destination, name_file))
 
             if folder == 'archives':
                 unpack_archive(os.path.join(
                     folder_destination, name_file))
+            unknown_extensions = False
+
+    if unknown_extensions:
+        folder_destination = os.path.join(path_root, 'unknown extensions')
+        try:
+            os.mkdir(os.path.join(folder_destination), mode=0o777, dir_fd=None)
+            shutil.move(path, os.path.join(folder_destination, name_file))
+        except FileExistsError:
+            shutil.move(path, os.path.join(folder_destination, name_file))
 
 
 def search_for_items(path, reset=True):
 
-    global list_files, empty_folders
+    global list_files
 
     if reset:
         list_files = []
-        empty_folders = []
 
     get_items = os.listdir(path)
 
     for item in get_items:
 
         if os.path.isdir(os.path.join(path, item)):
-
-            if not os.listdir(os.path.join(path, item)):
-                empty_folders.append(os.path.join(path, item))
             search_for_items(os.path.join(path, item), reset=False)
 
         else:
             list_files.append(os.path.join(path, item))
 
-    return [list_files, empty_folders]
+    return list_files
 
 
 def unpack_archive(path):
@@ -123,12 +125,12 @@ path = sys.argv[1]
 
 if os.path.exists(path):
 
-    path_files, path_empty_folders = search_for_items(path)
+    path_root = path
+    path_files = search_for_items(path)
     for item in path_files:
         sorted_files(item)
 
-    if path_empty_folders:
-        delete_empty_folder(path_empty_folders)
+    delete_folder(path_root)
 
 else:
     print('Not correct path')
