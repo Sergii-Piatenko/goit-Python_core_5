2aa1a5a1ddab716ab87e4b3c148fcd3be9f7687e
Serhii-Piatenko <94806482+Sergii-Piatenko@users.noreply.github.com>
2022-04-10 14:35:19 +0300

    Fixed

--- hw7/clean_folder/setup.py
+++ hw7/clean_folder/setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_namespace_packages
 
+
 setup(
     name='clean',
-    version='1',
+    version='0.0.1',
     description='Folder parsing script',
     url='https://github.com/Sergii-Piatenko/goit-Python_core_5/tree/main/hw7/clean_folder',
     author='Serhii Piatenko',
     author_email='sergiy.pyatenko@gmail.com',
     license='MIT',
+    packages=find_namespace_packages(),
     entry_points={'console_scripts': [
-        'clean-folder = useful.some_code:clean']}
+        'clean-folder = clean_folder.clean:main']}
 )
